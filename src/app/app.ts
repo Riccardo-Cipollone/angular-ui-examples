023edf94518ef6fc2ca775d68e896db4433bf6a8
riccardo_cipollone <r.cipollone@softwareindustriale.it>
2025-07-02 17:19:44 +0200

    added outputs to handle events from parent.  Added array of messages to render multiple components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatBubble } from './components/chat-bubble';
+
+export type ChatItem = {
+  text: string;
+  button: boolean;
+  buttonClick?: () => void;
+  variant?: 'primary' | 'secondary' | 'accent';
+  align?: 'start' | 'end';
+};
 
 @Component({
   selector: 'app-root',
   imports: [ChatBubble],
   template: `
     <div class="max-w-screen-sm mx-auto">
-      <app-chat-bubble>
+      <!-- <app-chat-bubble (buttonClickEmitter)="onButtonClickHandler($event)">
         <em>Lorem ipsum dolor sit.</em>
       </app-chat-bubble>
-      <app-chat-bubble align="end" variant="secondary">
+      <app-chat-bubble
+        align="end"
+        variant="secondary"
+        (buttonClickEmitter)="onButtonClickHandler($event)"
+      >
         <strong>Grassetto!</strong>
       </app-chat-bubble>
-      <app-chat-bubble variant="accent" />
+      <app-chat-bubble variant="accent" [button]="false" /> -->
+      @for (msg of chat(); track $index) {
+      <app-chat-bubble
+        [align]="msg.align"
+        [variant]="msg.variant"
+        [button]="msg.button"
+        (buttonClickEmitter)="onButtonClickHandler($event, $index)"
+      >
+        {{ msg.text }}
+      </app-chat-bubble>
+      }
     </div>
   `,
   styles: [],
 })
 export class App {
-  protected title = 'angular-ui-examples';
+  chat = signal<ChatItem[]>([
+    {
+      text: 'Hello World!',
+      button: true,
+    },
+    {
+      text: 'Fine Thanks!',
+      variant: 'secondary',
+      align: 'end',
+      button: true,
+    },
+    {
+      text: 'Great!',
+      variant: 'accent',
+      align: 'start',
+      button: false,
+    },
+  ]);
+
+  onButtonClickHandler(event: unknown, index: number) {
+    console.log(`Button clicked: ${index}`, event);
+  }
 }
--- src/app/components/chat-bubble.ts
+++ src/app/components/chat-bubble.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-chat-bubble',
@@ -8,6 +8,24 @@ import { Component, computed, input } from '@angular/core';
       <div class="chat-bubble" [class]="classVariant()">
         <!-- {{ title() }} -->
         <ng-content>Default content</ng-content>
+        @if (button()) {
+        <button class="btn ml-4" (click)="doSomething()">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2.5"
+            stroke="currentColor"
+            class="size-[1.2em]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+            />
+          </svg>
+        </button>
+        }
       </div>
     </div>
   `,
@@ -16,6 +34,8 @@ export class ChatBubble {
   // title = input('...');
   align = input<AlignType>('start');
   variant = input<Variant>('primary');
+  button = input<boolean>(true);
+  buttonClickEmitter = output();
 
   classAlign = computed(() =>
     this.align() === 'start' ? 'chat-start' : 'chat-end'
@@ -29,9 +49,15 @@ export class ChatBubble {
         return 'chat-bubble-secondary';
       case 'accent':
         return 'chat-bubble-accent';
+      default:
+        return 'chat-bubble-primary';
     }
   });
+
+  doSomething() {
+    this.buttonClickEmitter.emit();
+  }
 }
 
-type AlignType = 'start' | 'end';
-type Variant = 'primary' | 'secondary' | 'accent';
+type AlignType = 'start' | 'end' | undefined;
+type Variant = 'primary' | 'secondary' | 'accent' | undefined;
